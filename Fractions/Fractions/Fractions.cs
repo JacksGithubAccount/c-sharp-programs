14b24e7e09fbc3681ee9986a22c95b3d94294c72
Jack <jckzhao@yahoo.com>
2018-03-20 19:34:12 -0400

    a project that performs basic arithmetic functions with fractions

---
+++ Fractions/Fractions/Fractions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fractions
+{
+    class Fractions
+    {
+        private int numerator;
+        private int denominator;
+
+        public int numeratorProps
+        {
+            set
+            {
+                numerator = value;
+            }
+            get
+            {
+                return numerator;
+            }
+        }
+        public int denominatorProps
+        {
+            set
+            {
+                denominator = value;
+            }
+            get
+            {
+                return denominator;
+            }
+        }
+
+        public Fractions(int num, int denom)
+        {
+            numerator = num;
+            denominator = denom;
+        }
+        public Fractions()
+        {
+        }
+
+        public Fractions addition(Fractions second)
+        {
+            string ops = "Addition";
+            Fractions answer = new Fractions();
+            answer.numerator = this.numerator * second.denominator + second.numerator * this.denominator;
+            answer.denominator = this.denominator * second.denominator;
+
+            answer = this.reduce(answer);
+            this.print(answer, ops);
+
+            return answer;
+        }
+        public Fractions subtract(Fractions second)
+        {
+            string ops = "Subtraction";
+            Fractions answer = new Fractions();
+            answer.numerator = this.numerator * second.denominator - second.numerator * this.denominator;
+            answer.denominator = this.denominator * second.denominator;
+
+            answer = this.reduce(answer);
+            this.print(answer, ops);
+            return answer;
+        }
+        public Fractions multiply(Fractions second)
+        {
+            string ops = "Multiplication";
+            Fractions answer = new Fractions();
+            answer.numerator = this.numerator * second.numerator;
+            answer.denominator = this.denominator * second.denominator;
+
+            answer = this.reduce(answer);
+            this.print(answer, ops);
+            return answer;
+        }
+        public Fractions divide(Fractions second)
+        {
+            string ops = "Division";
+            Fractions answer = new Fractions();
+            answer.numerator = this.numerator * second.denominator;
+            answer.denominator = second.numerator * this.denominator;
+
+            answer = this.reduce(answer);
+            this.print(answer, ops);
+            return answer;
+        }
+        private int GCD(int x, int y)
+        {
+            int temp;
+            while (y != 0)
+            {
+                temp = y;
+                y = x % y;
+                x = temp;
+            }
+            return x;
+        }
+        private Fractions reduce(Fractions x)
+        {
+            int gcd;
+
+            gcd = GCD(x.numerator, x.denominator);
+
+            x.numerator = x.numerator / gcd;
+            x.denominator = x.denominator / gcd;
+
+            if (x.denominator < 1)
+            {
+                x.denominator = x.denominator * -1;
+                x.numerator = x.numerator * -1;
+            }
+            return x;
+        }
+        private void print(Fractions derp, string operant)
+        {
+            Console.WriteLine(operant + ": " + derp.numerator + "/" + derp.denominator);
+        }
+
+    }
+}
---
+++ Fractions/Fractions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fractions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string numString;
+            string denomString;
+            string numstring2;
+            string denomString2;
+            Fractions secondFrac, firstFrac;
+            Fractions answer;
+            int numInt, numInt2, denomInt, denomInt2;
+            
+            //input numbers for fractions
+            Console.WriteLine("Input first Numerator:");
+            numString = Console.ReadLine();                           
+            Console.WriteLine("Input first Denominator:");
+            denomString = Console.ReadLine();                
+            Console.WriteLine("Input second Numerator:");
+            numstring2 = Console.ReadLine();
+            Console.WriteLine("Input second Denominator:");
+            denomString2 = Console.ReadLine();
+
+            //parse the numbers
+                numInt = int.Parse(numString);
+                numInt2 = int.Parse(numstring2);
+
+                denomInt = int.Parse(denomString);
+                denomInt2 = int.Parse(denomString2);
+            
+            
+            //display fractions
+            firstFrac = new Fractions(numInt, denomInt);
+            secondFrac = new Fractions(numInt2, denomInt2);
+            answer = firstFrac.addition(secondFrac);           
+            answer = firstFrac.subtract(secondFrac);
+            answer = firstFrac.multiply(secondFrac);
+            answer = firstFrac.divide(secondFrac);
+
+            Console.ReadKey();
+
+        }
+        }
+}
+
