de4a3328dce2e77b40a3ee5232e1bfb2dc3c3fd8
Jack <jckzhao@yahoo.com>
2018-03-20 19:29:32 -0400

    a programs that checks if a string is a palindrome

---
+++ Pallendromes/Pallendromes/PallendromeCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pallendromes
+{
+    class PallendromeCheck
+    {
+        string checkString = "";
+        public static void spaceStripper()
+        {
+    
+        }
+        public string recursiveChecking(string sentence, int n)
+        {
+            string derp = "";
+            if (sentence.Length == 1 || sentence.Length == 0)
+            {
+                derp = "true";
+                return derp;
+            }
+            else if (sentence.StartsWith("") != sentence.EndsWith(""))
+            {
+                derp = "false";
+                return derp;
+            }
+            else
+            {
+                sentence = sentence.Remove(0, 1);
+                sentence = sentence.Remove(sentence.Length-1,1);
+                return recursiveChecking(recursiveChecking(sentence, n+1), n+1);
+            }
+ 
+        }
+        public PallendromeCheck(string sentence)
+        {
+            checkString = sentence.Trim().ToLower();
+            recursiveChecking(checkString, 0);
+        }
+    }
+}
---
+++ Pallendromes/Pallendromes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pallendromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string checkString;
+            Console.WriteLine("Enter a string value here: ");
+            checkString = Console.ReadLine();
+            PallendromeCheck derp = new PallendromeCheck(checkString);
+            Console.ReadKey();
+        }
+    }
+}
