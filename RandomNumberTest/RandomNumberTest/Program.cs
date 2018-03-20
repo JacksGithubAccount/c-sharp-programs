c89ccd61bb4d02e04a3e56f01578d66c2c265433
Jack <jckzhao@yahoo.com>
2018-03-20 19:25:43 -0400

    program that generates a bunch of numbers infinitely

---
+++ RandomNumberTest/RandomNumberTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RandomNumberTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndValue = new Random();
+
+            bool check = false;
+            do
+            {
+                int rndNumber = rndValue.Next(1,9);
+                Console.WriteLine("Random Value is " + rndNumber);
+                
+            
+            }while(check == false);
+
+
+        }
+    }
+}
